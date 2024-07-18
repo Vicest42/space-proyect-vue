90e7b9e996135d18b55ae0bb8c529f18b78c73e5
alanaviles12 <143649852+alanaviles12@users.noreply.github.com>
2024-07-18 00:55:04 -0600

    se crea el componente de astro para renderizar los datos

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <script setup>
-
+import Header from "@/components/Header.vue";
+import {RouterView} from "vue-router";
 </script>
 
 <template>
- <h1>prueba</h1>
-</template>
+  <Header />
 
+  <main class="container mx-auto mt-10">
+    <RouterView />
+  </main>
+</template>
---
+++ src/components/Astro.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { useAstrosStore } from '@/stores/astros.js';
+import {computed} from "vue";
+
+const props = defineProps({
+  astro: {
+    type: Object,
+    required: true,
+  },
+});
+
+const store = useAstrosStore();
+const imageUrl = computed(() => store.images[props.astro.englishName] || '/img/celestial-bodies/default.png');
+</script>
+
+<template>
+  <div class="border shadow-lg">
+    <div>
+      <img
+          :src="imageUrl"
+          alt="Imagen de astro"
+      >
+    </div>
+
+    <div class="p-5">
+      <h2 class="text-2xl truncate font-extrabold">{{ astro.englishName }}</h2> <!-- Muestra el nombre del astro -->
+      <!-- Agrega más detalles aquí según sea necesario -->
+    </div>
+  </div>
+</template>
