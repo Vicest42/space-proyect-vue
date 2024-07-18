f2ba0ea6c7f5e983efbf29871b55b0ae4eea21e0
Vicest42 <quiqueecheverria10@hotmail.com>
2024-07-18 00:51:06 -0600

    se configuran las API

---
+++ src/services/APIService.js
@@ -0,0 +1,11 @@
+import api from "@/lib/axios.js";
+
+export default {
+
+    catalogoAstros(){
+        return api('/')
+    },
+    buscarAstros(busqueda){
+        return api(/${busqueda.nombre})
+    }
+};
